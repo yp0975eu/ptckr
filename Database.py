5607ebab2475a57b825cad2a162fbb7e9752b9d6
yp0975eu(Branden) <yp0975eu@users.noreply.github.com>
2016-09-13 11:42:12 -0500

    Initial commit

---
+++ Database.py
@@ -0,0 +1,100 @@
+import sqlite3
+
+
+class Db:
+    __connection = None
+    __cursor = None
+    __database_file_name = "projects.db"
+    __table_names = ("tracking", 'tasks', 'entries')
+
+    # setup project database
+    def __init__(self):
+        try:
+            self.__connection = sqlite3.connect(self.__database_file_name)
+            self.__cursor = self.__connection.cursor()
+        except sqlite3.DataError as data_error:
+            print("Cannot connect to database", data_error)
+            exit()
+
+    def setup_tables(self):
+        existing_tables = []
+        for table in self.__table_names:
+            if not self.table_exist(table):
+                try:
+                    # this calls the method named: self.make_table_<table_name>
+                    getattr(self, 'make_table_' + table)()
+                except AttributeError as a_error:
+                    print("Error making table. "
+                          "Does table '{}' have a corresponding method 'make_table_{}()' ?".format(table, table))
+                    print(a_error)
+            existing_tables.append(True)
+
+    def table_exist(self, table):
+        # SQL from http: // stackoverflow.com / a / 8827554
+        sql = "SELECT count(*) FROM sqlite_master WHERE type='table' AND name = ?;"
+        cur = self.__cursor
+        # turn into tuple
+        value = table,
+        cur.execute(sql, value)
+        count = cur.fetchall()[0][0]
+        return count == 1
+
+    # Tracking table keeps track of currently selected task, start and end time
+    def make_table_tracking(self):
+        sql = '''CREATE TABLE IF NOT EXISTS tracking (
+         start INTEGER NOT NULL,
+         stop INTEGER,
+         task_id INTEGER,
+         FOREIGN KEY(task_id) REFERENCES tasks(ROWID)
+         );'''
+        self.make_table(sql)
+
+    def make_table_tasks(self):
+        # id removed because sqlite3 makes an id automatically
+        # id          INTEGER NOT NULL PRIMARY KEY,
+        sql = '''CREATE TABLE  IF NOT EXISTS tasks  (
+         name             TEXT NOT NULL
+         );'''
+        self.make_table(sql)
+
+    def make_table_entries(self):
+        sql = '''CREATE TABLE  IF NOT EXISTS entries  (
+         start       TEXT NOT NULL,
+         stop        TEXT NOT NULL,
+         task_id     INTEGER,
+         description TEXT NOT NULL,
+         FOREIGN KEY(task_id) REFERENCES tasks(ROWID)
+         );'''
+        self.make_table(sql)
+
+    # runs sql with no arguments
+    def make_table(self, sql):
+        try:
+            cur = self.__cursor
+            cur.execute(sql)
+            self.__connection.commit()
+        except sqlite3.OperationalError as o_err:
+            print("Error Making Table", o_err)
+        except sqlite3.DatabaseError as db_err:
+            print("Database Error while making table", db_err)
+
+    def insert_project(self, project_name):
+        sql = """INSERT INTO projects(name) VALUES (?);"""
+        # Convert to tuple
+        values = project_name,
+        success_message = "Creating Project '{}'".format(project_name)
+        self.insert_sql(sql, values, success_message)
+
+
+    # runs sql with arguments
+    def insert_sql(self, sql, values, success_message):
+        try:
+            self.__cursor.execute(sql, values)
+            self.__connection.commit()
+
+            # if there is no error then print the success message
+            print(success_message)
+        except sqlite3.OperationalError as o_err:
+            print("Error Making Table", o_err)
+        except sqlite3.IntegrityError:
+            print("Project '{}' already exists".format(values[0]))
---
+++ ptckr.py
@@ -0,0 +1,117 @@
+import sys
+from Database import Db
+
+FILENAME = 0
+COMMAND = 1
+FIRST_ARG = 2
+SECOND_ARG = 3
+
+
+class Menu:
+    top_menu = (
+        {'command': "init", "desc": '\tinit\n\t\tDef: Initializes a project in current directory'},
+        {'command': "help", "desc": '\thelp\n\t\tDef: Shows this help menu'},
+        {'command': 'create', 'desc': '\tcreate <task>\n\t\tDef: Creates a new task'},
+        {'command': 'select', 'desc': '\tselect <task>\n\t\tDef: Select a task to track'},
+        {'command': 'status', 'desc': '\tstatus\n\t\tDef: Shows current selected task'},
+        {'command': 'start', 'desc': '\tstart\n\t\tDef: Starts tracking time'},
+        {'command': 'stop', 'desc': '\tstop\n\t\tDef: Stops tracking time'},
+        {'command': "view", "desc": '\tview [-t]\n\t\tDef: Shows entries for currently selected task.'
+                                      ' Use -t to view all tasks'},
+    )
+    project_menu_options = ['Start Time', 'End Time', 'View Entries ']
+
+    @staticmethod
+    def show_help():
+        print('\n{0} : {1}'.format('command'.ljust(10), 'usage'))
+        print('-----------------------------------------------')
+        for option in Menu.top_menu:
+            print('{0} : {1}'.format(option['command'].ljust(10), option['desc']))
+        quit()
+
+
+if __name__ == '__main__':
+    db = Db()
+    if len(sys.argv) >= 2:
+        if sys.argv[COMMAND] == 'init':
+
+            # calling multiple times does nothing
+            if len(sys.argv) == 2:
+                print("Project initialized.")
+            else:
+                # display error if we have more args then needed
+                print("Incorrect number of arguments")
+                Menu.show_help()
+
+        elif sys.argv[COMMAND] == 'help':
+
+            Menu.show_help()
+
+        elif sys.argv[COMMAND] == 'create':
+
+            if len(sys.argv) == 3:
+
+                task_name = sys.argv[FIRST_ARG]
+                print('Creating task {}'.format(task_name))
+
+            else:
+
+                Menu.show_help()
+
+        elif sys.argv[COMMAND] == 'select':
+
+            if len(sys.argv) == 3:
+
+                task_name = sys.argv[FIRST_ARG]
+                print('Selecting task {}'.format(task_name))
+
+            else:
+
+                Menu.show_help()
+
+        elif sys.argv[COMMAND] == 'status':
+
+            if len(sys.argv) == 2:
+
+                print('Show current selected task here')
+
+            else:
+
+                Menu.show_help()
+
+        elif sys.argv[COMMAND] == 'start':
+
+            if len(sys.argv) == 2:
+
+                # if a task is selected then
+                print('Start tracking time for currently selected task')
+
+            else:
+
+                Menu.show_help()
+
+        elif sys.argv[COMMAND] == 'stop':
+
+            if len(sys.argv) == 2:
+
+                # if a task is selected then
+                print('Stop tracking time for currently selected task')
+                print('Enter Message for currently selected task')
+                print('Message saved entry header')
+
+            else:
+
+                Menu.show_help()
+
+        elif sys.argv[COMMAND] == 'view':
+            if len(sys.argv) == 2:
+                print('Viewing all entries under currently selected task')
+            elif len(sys.argv) == 3 and sys.argv[FIRST_ARG] == "-t":
+                print('view all tasks')
+            else:
+                Menu.show_help()
+        else:
+            print("Don't know that one. Try one of these:")
+            Menu.show_help()
+    elif len(sys.argv) <= 1:
+        Menu.show_help()
