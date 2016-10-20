d03b5c4443975b8e6b8f58ff1a735185a7025004
yp0975eu(Branden) <yp0975eu@users.noreply.github.com>
2016-10-19 21:27:20 -0500

    added get methods to intantiated task attributes

--- tasks.py
+++ tasks.py
@@ -1,17 +1,15 @@
 from database import *
+import datetime
 
 
 class Task(Database):
 
-    __row_id = None
-    __name = None
-
     def __init__(self):
         Database.__init__(self)
-
-    def get_all_tasks(self):
-        # select * from tasks
-        pass
+        self._row_id = None
+        self._name = None
+        self._created_at = None
+        self._updated_at = None
 
     def get_task_by_id(self, task_id):
         sql = """SELECT rowid, name FROM tasks WHERE rowid = (?);"""
@@ -20,13 +18,13 @@ class Task(Database):
         row = self.select_one_sql(sql, values)
 
         if row:
-            self.__row_id = row['rowid']
-            self.__name = row['name']
+            self.set_task_attributes(row)
 
     def create_task(self, task_name):
-        sql = """INSERT INTO tasks(name) VALUES (?);"""
+        sql = """INSERT INTO tasks(name, created_at, updated_at) VALUES (?,?,?);"""
         # Convert to tuple
-        values = task_name,
+        created_at = updated_at = datetime.datetime.now()
+        values = (task_name, created_at, updated_at)
         success_message = "Creating Task '{}'".format(task_name)
         self.insert_sql(sql, values, success_message)
 
@@ -34,20 +32,35 @@ class Task(Database):
         pass
 
     def get_task_by_name(self, name):
-        sql = """SELECT rowid, name FROM tasks WHERE name = (?);"""
+        sql = """SELECT rowid, name, created_at, updated_at FROM tasks WHERE name = (?);"""
         # Convert to tuple
         values = name,
         row = self.select_one_sql(sql, values)
 
         if row:
-            self.__row_id = row['rowid']
-            self.__name = row['name']
+            self.set_task_attributes(row)
+
+    def set_task_attributes(self, row):
+        self._row_id = row['rowid']
+        self._name = row['name']
+        self._created_at = row['created_at']
+        self._updated_at = row['updated_at']
+
+    def get_row_id(self):
+        return self._row_id
+
+    def get_name(self):
+        return self._name
 
     def is_loaded(self):
-        return self.__row_id is not None if True else False
+        return self._row_id is not None if True else False
 
     def __str__(self):
         if self.is_loaded():
-            return "ID   : {0}\nName : {1}".format(self.__row_id, self.__name)
+            return "\nID   : {0}\nName : {1}\nCreated at: {2}\nUpdated at: {3}".format(
+                self._row_id,
+                self._name,
+                self._created_at,
+                self._updated_at)
         else:
             return "No Task Loaded"
